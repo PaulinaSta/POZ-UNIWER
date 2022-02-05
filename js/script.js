cb25990837a8e0b6df6f9b4f94e6ff011ed63b99
Paulina <paulinaa.staszynska@gmail.com>
2022-02-05 17:04:01 +0100

    Added js and contact form

--- js/script.js
+++ js/script.js
@@ -12,9 +12,35 @@ const burgerLine1 = document.querySelector('.burger__line--line1')
 const burgerLine2 = document.querySelector('.burger__line--line2')
 const burgerLine3 = document.querySelector('.burger__line--line3')
 
+const footerYear = document.querySelector('.footer-year')
+
+const nav = document.querySelector('.navbar-collapse')
+const menuAllLinks = document.querySelectorAll('.nav-link')
+
+const setCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+
+setCurrentYear()
+
 burger.addEventListener('click', function () {
 	burger.classList.toggle('active')
 	burgerLine1.classList.toggle('js-burger__line1--active')
 	burgerLine2.classList.toggle('js-burger__line2--active')
 	burgerLine3.classList.toggle('js-burger__line3--active')
 })
+
+const hideNav = () => {
+	menuAllLinks.forEach(link => {
+		link.addEventListener('click', () => {
+			nav.classList.remove('show')
+			burger.classList.remove('active')
+			burgerLine1.classList.remove('js-burger__line1--active')
+			burgerLine2.classList.remove('js-burger__line2--active')
+			burgerLine3.classList.remove('js-burger__line3--active')
+		})
+	})
+}
+
+hideNav()
